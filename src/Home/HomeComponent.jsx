36b9d306b339fc104e0a24e4c987625838515d66
ajeshb <ajesh.16.09@gmail.com>
2018-08-07 04:18:32 +0530

    first part completed

--- src/Home/HomeComponent.jsx
+++ src/Home/HomeComponent.jsx
@@ -1,12 +1,37 @@
 import React from "react";
 import { Invitation } from "./InvitationComponent";
+import WeddingImage from "../Images/wedding-profile.png";
+import "./HomeComponent.css";
 
 class HomeComponent extends React.Component {
   render() {
     return (
       <div>
-        <h4>Home</h4>
-        <Invitation />
+        <div className="App-header">
+          <div className="logo">
+            <span className="first-name">J</span><span className="and">&</span><span className="second-name">M</span>
+          </div>
+          <div className="header-buttons">
+            <button className="header-button" >Save The Date</button>
+            <button className="header-button" >Out Story</button>
+            <button className="header-button" >Reach Us</button>
+          </div>
+        </div>
+        <div className="first-part" >
+          <div className="profile-image">
+            <img src={WeddingImage} alt="wedding-image" />
+          </div>
+          <div className="wedding-heading">
+            <p> Jithin & Mahima</p>
+            <p> Wedding</p>
+          </div>
+        </div>
+        <div className="second-part" >
+        </div>
+        <div className="third-part" >
+        </div>
+        <div className="App-footer">
+        </div>
       </div>
     );
   }
